afcf733db4867ea7652979c69afd0443bed9f063
dpsus4 <fradelpo@yahoo.com>
2020-10-09 16:30:50 +0200

    After additional changes for offers

--- src/app/places/places.service.ts
+++ src/app/places/places.service.ts
@@ -26,11 +26,43 @@ export class PlacesService {
       "Not your avarage city trip!",
       "https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcSdhpjgn2kgR2H77aKf6avPxVVA71Rxz8io-w&usqp=CAU",
       99.99
+    )
+  ];
+
+  private _offers: Place[] = [
+    new Place(
+      "p1",
+      "Manhattan Mansion Offer",
+      "In the heart of New York City",
+      "https://media.gettyimages.com/photos/aerial-view-of-lower-manhattan-new-york-picture-id946087016?k=6&m=946087016&s=612x612&w=0&h=FPq454ti8ZKPiMzyDPJ_A4BNQaN-2olcfg4TYgMFR1w=",
+      99.99
     ),
+    new Place(
+      "p2",
+      "L'Amour Toujours Offer",
+      "A romantic place in Paris!",
+      "https://upload.wikimedia.org/wikipedia/commons/4/4b/La_Tour_Eiffel_vue_de_la_Tour_Saint-Jacques%2C_Paris_ao%C3%BBt_2014_%282%29.jpg",
+      129.99
+    ),
+    new Place(
+      "p3",
+      "The Foggy Palace Offer",
+      "Not your avarage city trip!",
+      "https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcSdhpjgn2kgR2H77aKf6avPxVVA71Rxz8io-w&usqp=CAU",
+      59.99
+    )
   ];
 
   get places(): Place[] {
     return [...this._places];
+  }
+
+  get offers(): Place[] {
+    return [...this._offers];
+  }
+
+  getPlace(id: string) {
+    return { ...this._places.find((p) => p.id === id) };
   }
 
   constructor() {}
