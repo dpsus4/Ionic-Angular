cca01c5727300d038d2cecc8f89e04e005cf46f6
dpsus4 <fradelpo@yahoo.com>
2020-10-28 12:56:04 +0100

    Added select option control

--- src/app/places/offers/edit-offer/edit-offer.module.ts
+++ src/app/places/offers/edit-offer/edit-offer.module.ts
@@ -1,19 +1,30 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { FormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 import { IonicModule } from '@ionic/angular';
 
 import { EditOfferPageRoutingModule } from './edit-offer-routing.module';
 
 import { EditOfferPage } from './edit-offer.page';
+import { RouterModule, Routes } from '@angular/router';
+// import { Route } from '@angular/compiler/src/core';
+
+const routes: Routes = [
+  {
+    path: '',
+    component: EditOfferPage
+  }
+];
 
 @NgModule({
   imports: [
     CommonModule,
-    FormsModule,
+    // FormsModule,
     IonicModule,
-    EditOfferPageRoutingModule
+    ReactiveFormsModule,
+    // EditOfferPageRoutingModule
+    RouterModule.forChild(routes)
   ],
   declarations: [EditOfferPage]
 })
--- src/app/places/offers/edit-offer/edit-offer.page.ts
+++ src/app/places/offers/edit-offer/edit-offer.page.ts
@@ -1,33 +1,57 @@
-import { Component, OnInit } from '@angular/core';
-import { ActivatedRoute } from '@angular/router';
-import { NavController } from '@ionic/angular';
-import { Place } from '../../place.model';
-import { PlacesService } from '../../places.service';
+import { Component, OnInit } from "@angular/core";
+import { FormGroup, FormControl, Validators } from "@angular/forms";
+import { ActivatedRoute } from "@angular/router";
+import { NavController } from "@ionic/angular";
+import { Place } from "../../place.model";
+import { PlacesService } from "../../places.service";
 
 @Component({
-  selector: 'app-edit-offer',
-  templateUrl: './edit-offer.page.html',
-  styleUrls: ['./edit-offer.page.scss'],
+  selector: "app-edit-offer",
+  templateUrl: "./edit-offer.page.html",
+  styleUrls: ["./edit-offer.page.scss"],
 })
 export class EditOfferPage implements OnInit {
- place: Place;
+  place: Place;
+  form: FormGroup;
 
-  constructor(private route: ActivatedRoute, private navCtrl: NavController, private placesService: PlacesService) { }
+  constructor(
+    private route: ActivatedRoute,
+    private navCtrl: NavController,
+    private placesService: PlacesService
+  ) {}
 
   ngOnInit() {
     this.route.paramMap.subscribe((paramMap) => {
       if (!paramMap.has("placeId")) {
-        this.navCtrl.navigateBack('/places/tabs/offers');
+        this.navCtrl.navigateBack("/places/tabs/offers");
 
         return;
       }
 
-      this.place = this.placesService.getPlace(paramMap.get('placeId'));
+      this.place = this.placesService.getPlace(paramMap.get("placeId"));
+      this.form = new FormGroup({
+        title: new FormControl(this.place.title, {
+          updateOn: "blur",
+          validators: [Validators.required],
+        }),
+        description: new FormControl(this.place.description, {
+          updateOn: "blur",
+          validators: [Validators.required, Validators.maxLength(180)],
+        }),
+      });
     });
   }
 
   onApply() {
-    this.navCtrl.navigateBack('/places/tabs/offers');
+    this.navCtrl.navigateBack("/places/tabs/offers");
   }
 
+  onUpdateOffer() {
+    if (!this.form.valid)
+    {
+      return;
+    }
+
+    console.log(this.form);
+  }
 }
