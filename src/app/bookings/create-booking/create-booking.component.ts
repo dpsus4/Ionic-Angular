c78f0fbbe8d141473bf60542e238e12231ba678c
dpsus4 <fradelpo@yahoo.com>
2020-10-28 23:03:57 +0100

    Added date controls

--- src/app/bookings/create-booking/create-booking.component.ts
+++ src/app/bookings/create-booking/create-booking.component.ts
@@ -1,24 +1,49 @@
-import { Component, Input, OnInit } from '@angular/core';
-import { ModalController } from '@ionic/angular';
-import { Place } from 'src/app/places/place.model';
+import { Component, Input, OnInit } from "@angular/core";
+import { ModalController } from "@ionic/angular";
+import { Place } from "src/app/places/place.model";
 
 @Component({
-  selector: 'app-create-booking',
-  templateUrl: './create-booking.component.html',
-  styleUrls: ['./create-booking.component.scss'],
+  selector: "app-create-booking",
+  templateUrl: "./create-booking.component.html",
+  styleUrls: ["./create-booking.component.scss"],
 })
 export class CreateBookingComponent implements OnInit {
   @Input() selectedPlace: Place;
+  @Input() selectedMode: "select" | "random";
 
-  constructor(private modalCtrl: ModalController) { }
+  startDate: string;
+  endDate: string;
 
-  ngOnInit() {}
+  constructor(private modalCtrl: ModalController) {}
+
+  ngOnInit() {
+    const availableFrom = new Date(this.selectedPlace.availableFrom);
+    const availableTo = new Date(this.selectedPlace.availableTo);
+
+    if (this.selectedMode === "random") {
+      this.startDate = new Date(
+        availableFrom.getTime() +
+          Math.random() *
+            (availableTo.getTime() -
+              7 * 24 * 60 * 60 * 1000 -
+              availableFrom.getTime())
+      ).toISOString();
+
+      this.endDate = new Date(
+        new Date(this.startDate).getTime() +
+          Math.random() *
+            (new Date(this.startDate).getTime() +
+              6 * 24 * 60 * 60 * 1000 -
+              new Date(this.startDate).getTime())
+      ).toISOString();
+    }
+  }
 
   onCancel() {
-    this.modalCtrl.dismiss(null, 'cancel');
+    this.modalCtrl.dismiss(null, "cancel");
   }
 
   onBookPlace() {
-    this.modalCtrl.dismiss({message: 'This is a dummy message!'}, 'confirm');
+    this.modalCtrl.dismiss({ message: "This is a dummy message!" }, "confirm");
   }
 }
--- src/app/places/places.service.ts
+++ src/app/places/places.service.ts
@@ -11,22 +11,28 @@ export class PlacesService {
       "Manhattan Mansion",
       "In the heart of New York City",
       "https://media.gettyimages.com/photos/aerial-view-of-lower-manhattan-new-york-picture-id946087016?k=6&m=946087016&s=612x612&w=0&h=FPq454ti8ZKPiMzyDPJ_A4BNQaN-2olcfg4TYgMFR1w=",
-      149.99
+      149.99,
+      new Date("2019-01-01"),
+      new Date("2019-12-31")
     ),
     new Place(
       "p2",
       "L'Amour Toujours",
       "A romantic place in Paris!",
       "https://upload.wikimedia.org/wikipedia/commons/4/4b/La_Tour_Eiffel_vue_de_la_Tour_Saint-Jacques%2C_Paris_ao%C3%BBt_2014_%282%29.jpg",
-      189.99
+      189.99,
+      new Date("2019-01-01"),
+      new Date("2019-12-31")
     ),
     new Place(
       "p3",
       "The Foggy Palace",
       "Not your avarage city trip!",
       "https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcSdhpjgn2kgR2H77aKf6avPxVVA71Rxz8io-w&usqp=CAU",
-      99.99
-    )
+      99.99,
+      new Date("2019-01-01"),
+      new Date("2019-12-31")
+    ),
   ];
 
   private _offers: Place[] = [
@@ -35,22 +41,28 @@ export class PlacesService {
       "Manhattan Mansion Offer",
       "In the heart of New York City",
       "https://media.gettyimages.com/photos/aerial-view-of-lower-manhattan-new-york-picture-id946087016?k=6&m=946087016&s=612x612&w=0&h=FPq454ti8ZKPiMzyDPJ_A4BNQaN-2olcfg4TYgMFR1w=",
-      99.99
+      99.99,
+      new Date("2019-01-01"),
+      new Date("2019-12-31")
     ),
     new Place(
       "p2",
       "L'Amour Toujours Offer",
       "A romantic place in Paris!",
       "https://upload.wikimedia.org/wikipedia/commons/4/4b/La_Tour_Eiffel_vue_de_la_Tour_Saint-Jacques%2C_Paris_ao%C3%BBt_2014_%282%29.jpg",
-      129.99
+      129.99,
+      new Date("2019-01-01"),
+      new Date("2019-12-31")
     ),
     new Place(
       "p3",
       "The Foggy Palace Offer",
       "Not your avarage city trip!",
       "https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcSdhpjgn2kgR2H77aKf6avPxVVA71Rxz8io-w&usqp=CAU",
-      59.99
-    )
+      59.99,
+      new Date("2019-01-01"),
+      new Date("2019-12-31")
+    ),
   ];
 
   get places(): Place[] {
