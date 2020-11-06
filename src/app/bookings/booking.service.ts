0d968ca690aed8df2c4ca2c8246750c983d5b540
Francesco Del Pozzo <f.delpozzo@intranet.sedapta.com>
2020-11-06 18:34:56 +0100

    Additional changes booking places

--- src/app/bookings/booking.service.ts
+++ src/app/bookings/booking.service.ts
@@ -1,5 +1,6 @@
 import { Injectable } from '@angular/core';
 import { BehaviorSubject } from 'rxjs';
+import { delay, take, tap } from 'rxjs/operators';
 import { AuthService } from '../auth/auth.service';
 import { Booking } from './booking.model';
 
@@ -25,7 +26,11 @@ export class BookingService {
     }
 
     addBooking(placeId: string, placeTitle: string, placeImage: string, firstName: string, lastName: string, guestNumber: number, dateFrom: Date, dateTo: Date) {
-        const newBooking = new Booking(Math.random().toString(), placeId, this.authService.userId, placeTitle, firstName, lastName, guestNumber, dateFrom, dateTo);
+        const newBooking = new Booking(Math.random().toString(), placeId, this.authService.userId, placeTitle, placeImage, firstName, lastName, guestNumber, dateFrom, dateTo);
+
+        this.bookings.pipe(take(1), delay(1000), tap(bookings => {
+            this._bookings.next(bookings.concat(newBooking));
+        }));
     }
 
     cancelBooking(bookingId: string) {
