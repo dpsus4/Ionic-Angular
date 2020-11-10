0e3423479ff47323a00184b0d813dc4522472c74
dpsus4 <fradelpo@yahoo.com>
2020-11-10 22:03:26 +0100

    Web APIs fetch

--- src/app/places/offers/offers.page.ts
+++ src/app/places/offers/offers.page.ts
@@ -13,6 +13,7 @@ import { PlacesService } from "../places.service";
 export class OffersPage implements OnInit, OnDestroy {
   offers: Place[];
   private placesSub: Subscription;
+  isLoading: boolean = false;
 
   constructor(private placesService: PlacesService, private router: Router) {}
 
@@ -20,6 +21,13 @@ export class OffersPage implements OnInit, OnDestroy {
     // this.offers = this.placesService.offers;
     this.placesSub = this.placesService.places.subscribe((places) => {
       this.offers = places;
+    });
+  }
+
+  ionViewWillEnter() {
+    this.isLoading = true;
+    this.placesService.fetchPlaces().subscribe(() => {
+      this.isLoading = false;
     });
   }
 
--- src/app/places/places.service.ts
+++ src/app/places/places.service.ts
@@ -5,83 +5,29 @@ import { Place } from "./place.model";
 import { take, map, tap, delay, switchMap } from "rxjs/operators";
 import { HttpClient } from "@angular/common/http";
 
+interface PlaceData {
+  availableFrom: string;
+  availableTo: string;
+  description: string;
+  imageUrl: string;
+  price: number;
+  title: string;
+  userId: string;
+}
+
 @Injectable({
   providedIn: "root",
 })
 export class PlacesService {
-  private _places = new BehaviorSubject<Place[]>([
-    new Place(
-      "p1",
-      "Manhattan Mansion",
-      "In the heart of New York City",
-      "https://media.gettyimages.com/photos/aerial-view-of-lower-manhattan-new-york-picture-id946087016?k=6&m=946087016&s=612x612&w=0&h=FPq454ti8ZKPiMzyDPJ_A4BNQaN-2olcfg4TYgMFR1w=",
-      149.99,
-      new Date("2019-01-01"),
-      new Date("2019-12-31"),
-      "abc"
-    ),
-    new Place(
-      "p2",
-      "L'Amour Toujours",
-      "A romantic place in Paris!",
-      "https://upload.wikimedia.org/wikipedia/commons/4/4b/La_Tour_Eiffel_vue_de_la_Tour_Saint-Jacques%2C_Paris_ao%C3%BBt_2014_%282%29.jpg",
-      189.99,
-      new Date("2019-01-01"),
-      new Date("2019-12-31"),
-      "abc"
-    ),
-    new Place(
-      "p3",
-      "The Foggy Palace",
-      "Not your avarage city trip!",
-      "https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcSdhpjgn2kgR2H77aKf6avPxVVA71Rxz8io-w&usqp=CAU",
-      99.99,
-      new Date("2019-01-01"),
-      new Date("2019-12-31"),
-      "abc"
-    ),
-  ]);
-
-  private _offers: Place[] = [
-    new Place(
-      "p1",
-      "Manhattan Mansion Offer",
-      "In the heart of New York City",
-      "https://media.gettyimages.com/photos/aerial-view-of-lower-manhattan-new-york-picture-id946087016?k=6&m=946087016&s=612x612&w=0&h=FPq454ti8ZKPiMzyDPJ_A4BNQaN-2olcfg4TYgMFR1w=",
-      99.99,
-      new Date("2019-01-01"),
-      new Date("2019-12-31"),
-      "abc"
-    ),
-    new Place(
-      "p2",
-      "L'Amour Toujours Offer",
-      "A romantic place in Paris!",
-      "https://upload.wikimedia.org/wikipedia/commons/4/4b/La_Tour_Eiffel_vue_de_la_Tour_Saint-Jacques%2C_Paris_ao%C3%BBt_2014_%282%29.jpg",
-      129.99,
-      new Date("2019-01-01"),
-      new Date("2019-12-31"),
-      "abc"
-    ),
-    new Place(
-      "p3",
-      "The Foggy Palace Offer",
-      "Not your avarage city trip!",
-      "https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcSdhpjgn2kgR2H77aKf6avPxVVA71Rxz8io-w&usqp=CAU",
-      59.99,
-      new Date("2019-01-01"),
-      new Date("2019-12-31"),
-      "abc"
-    ),
-  ];
+  private _places = new BehaviorSubject<Place[]>([]);
 
   get places() {
     return this._places.asObservable();
   }
 
-  get offers(): Place[] {
-    return [...this._offers];
-  }
+  // get offers(): Place[] {
+  //   return [...this];
+  // }
 
   getPlace(id: string) {
     return this.places.pipe(
@@ -90,6 +36,41 @@ export class PlacesService {
         return { ...places.find((p) => p.id === id) };
       })
     );
+  }
+
+  fetchPlaces() {
+    return this.http
+      .get<{ [key: string]: PlaceData }>(
+        "https://ionic-angular-b73ed.firebaseio.com/offered-places.json"
+      )
+      .pipe(
+        map((resData) => {
+          const places = [];
+          for (const key in resData) {
+            if (resData.hasOwnProperty(key)) {
+              places.push(
+                new Place(
+                  key,
+                  resData[key].title,
+                  resData[key].description,
+                  resData[key].imageUrl,
+                  resData[key].price,
+                  new Date(resData[key].availableFrom),
+                  new Date(resData[key].availableTo),
+                  resData[key].userId
+                )
+              );
+            }
+          }
+
+          return places;
+          //return [];
+          // console.log(resData);
+        }),
+        tap((places) => {
+          this._places.next(places);
+        })
+      );
   }
 
   addPlace(
